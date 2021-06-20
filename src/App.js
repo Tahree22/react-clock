42b45c723e0e0619543f85d78bf065f67712adcd
Yinka <akinn.yinka@gmail.com>
2021-06-20 21:30:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:window.innerHeight}} className="flex w-full h-full justify-center items-center bg-black">
+      <div style={{backgroundColor:"#C4C4C4"}} className="w-52 h-52 rounded-full flex flex-col items-center justify-between p-2">
+        
+          <p className="">12</p>
+          <div className="flex justify-between items-center w-full">
+            <p>9</p>
+            <div style={{backgroundColor:"#FFF"}} className="w-2 h-2 rounded-full"/>
+            <p>3</p>
+            </div>
+          <p>6</p>
+        
+
+      </div>
+
     </div>
   );
 }
